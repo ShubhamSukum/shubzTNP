a366dd67113e6baa765ff17753974cc070abc382
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-10-12 19:28:59 +0530

    fixing✔

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import {GoogleOAuthProvider} from "@react-oauth/google";
-
-const client_Id=process.env.REACT_APP_ClientId;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GoogleOAuthProvider clientId={client_Id}>
     <App />
-  </GoogleOAuthProvider>
 );
