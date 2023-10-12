04e4a0d6ceca17a7fdd69903e55d8286ef89e191
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-10-12 19:22:03 +0530

    all sections WORKS✔

--- frontend/src/pages/C2021ppo.js
+++ frontend/src/pages/C2021ppo.js
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import "../App.css";
 
 export const C2021ppo = () => {
-  return (
-    <>
-      <center>
-        <h1 style={{marginBottom:"1vh"}}>2021 PPO</h1>
+  const [data, setData] = useState([]);
 
-        <div className='company-title-year'>
-            
+  useEffect(() => {
+    axios.get("http://localhost:3001/pict/2021ppo")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <center>
+      <h1 style={{ margin: "1vh" }}>2021 PPO</h1>
+      <div id="table-wrapper">
+        <div id="table-scroll">
+          <table>
+            <thead>
+              <tr>
+                <th className="sticky-header">Company</th>
+                <th className="sticky-header">Branch</th>
+                <th className="sticky-header">Total</th>
+                <th className="sticky-header">Salary</th>
+                <th className="sticky-header">Investment</th>
+              </tr>
+            </thead>
+
+            <tbody>
+                  {
+                    data.map((info)=>{
+                        return(
+                          <tr key={info._id}>
+                              <td>{info.company}</td>
+                              <td>{info.branch}</td>
+                              <td>{info.total}</td>
+                              <td>{info.salary}</td>
+                              <td>{info.investment}</td>
+                          </tr>
+                        )
+                    })
+                  }
+            </tbody>
+          </table>
         </div>
-      </center>
-  </>
+      </div>
+    </center>
   )
 }
--- frontend/src/pages/C2022.js
+++ frontend/src/pages/C2022.js
@@ -1,15 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import "../App.css";
 
 export const C2022 = () => {
-  return (
-    <>
-      <center>
-        <h1 style={{marginBottom:"1vh"}}>2022</h1>
+  const [data, setData] = useState([]);
 
-        <div className='company-title-year'>
-            
+  useEffect(() => {
+    axios.get("http://localhost:3001/pict/2022")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <center>
+      <h1 style={{ margin: "1vh" }}>2022</h1>
+      <div id="table-wrapper">
+        <div id="table-scroll">
+          <table>
+            <thead>
+              <tr>
+                <th className="sticky-header">Company</th>
+                <th className="sticky-header">Pointer</th>
+                <th className="sticky-header">Branch</th>
+                <th className="sticky-header">Total</th>
+                <th className="sticky-header">Salary</th>
+                <th className="sticky-header">Date</th>
+                <th className="sticky-header">Investment</th>
+              </tr>
+            </thead>
+
+            <tbody>
+                  {
+                    data.map((info)=>{
+                      const dateString = info.date;
+                      const dateObject = new Date(dateString);
+                      const year = dateObject.getFullYear();
+                      const month = dateObject.getMonth() + 1;
+                      const day = dateObject.getDate();
+
+                      const formattedDate = `${day < 10 ? '0' : ''}${day}-${month < 10 ? '0' : ''}${month}-${year}`;
+                        return(
+                          <tr key={info._id}>
+                              <td>{info.company}</td>
+                              <td>{info.pointer}</td>
+                              <td>{info.branch}</td>
+                              <td>{info.total}</td>
+                              <td>{info.salary}</td>
+                              <td>{formattedDate}</td>
+                              <td>{info.investment}</td>
+                          </tr>
+                        )
+                    })
+                  }
+            </tbody>
+          </table>
         </div>
-      </center>
-  </>
-  )
-}
+      </div>
+    </center>
+  );
+};
